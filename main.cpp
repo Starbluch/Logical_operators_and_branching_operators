2fc3cdfb80b1961da10efe9149b2ac37489bf83c
admin <gadzukoleksandr29@gmail.com>
2024-11-30 23:25:57 +0200

    commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include  <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num_1, num_2, num_3, num_4, num_5, num_6, num_7;
+    int max = 0;
+    cout << "number: "; cin >> num_1;
+    if (max < num_1) { max = num_1; }
+    
+    cout << "number: "; cin >> num_2;
+    if (max < num_2) { max = num_2; }
+    
+    cout << "number: "; cin >> num_3;
+    if (max < num_3) { max = num_3; }
+    
+    cout << "number: "; cin >> num_4;
+    if (max < num_4) { max = num_4; }
+    
+    cout << "number: "; cin >> num_5;
+    if (max < num_5) { max = num_5; }
+    
+    cout << "number: "; cin >> num_6;
+    if (max < num_6) { max = num_6; }
+    
+    cout << "number: "; cin >> num_7;
+    if (max < num_7) { max = num_7; }
+    
+    cout << max << endl;
+    
+    return 0;
+}
